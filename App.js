3ff6f44618b5c0bb5c0b769f97444e6f4d43c79b
Shawn Law <shawnlaw@tuta.io>
2022-11-10 09:07:48 +0800

    26.13 the auth flow is setup and working! omg

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
 
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+
+  const [accessToken, setAccessToken] = useState(null);
+  const [user, setUser] = useState(null)
+  
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: '427483537446-02egbblism33cet0caqbp4nbgrak897q.apps.googleusercontent.com',
+    webClientId: '427483537446-02egbblism33cet0caqbp4nbgrak897q.apps.googleusercontent.com',
+    iosClientId: 'x427483537446-rl4ab2uji8mi7olvbsqcf90ldacbr38r.apps.googleusercontent.com',
+    androidClientId: '427483537446-7a0f2ccbr7alflsufodff5uk9e074dk8.apps.googleusercontent.com',
+  });
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication: {accessToken} } = response;
+      console.log(accessToken);
+      setAccessToken(accessToken)
+
+      accessToken && fetchUserInfo();
+    }
+  }, [response, accessToken]);
+
+  async function fetchUserInfo() {
+    let response = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+
+    const userInfo = await response.json();
+    setUser(userInfo)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync();
+        }}
+      />
+      <Text>{ JSON.stringify(user) }</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-// web id 427483537446-02egbblism33cet0caqbp4nbgrak897q.apps.googleusercontent.com
+// web id 
 // web secret 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
-// ios x427483537446-rl4ab2uji8mi7olvbsqcf90ldacbr38r.apps.googleusercontent.com
-// android 427483537446-7a0f2ccbr7alflsufodff5uk9e074dk8.apps.googleusercontent.com
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -22,3 +64,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
