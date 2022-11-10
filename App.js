9a848136e7904f93b75010c6c9760fa1d3bb6e23
Shawn Law <shawnlaw@tuta.io>
2022-11-10 08:28:50 +0800

    install the needed dependencies

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -9,7 +10,10 @@ export default function App() {
     </View>
   );
 }
-
+// web id 427483537446-02egbblism33cet0caqbp4nbgrak897q.apps.googleusercontent.com
+// web secret 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
+// ios x427483537446-rl4ab2uji8mi7olvbsqcf90ldacbr38r.apps.googleusercontent.com
+// android 427483537446-7a0f2ccbr7alflsufodff5uk9e074dk8.apps.googleusercontent.com
 const styles = StyleSheet.create({
   container: {
     flex: 1,
